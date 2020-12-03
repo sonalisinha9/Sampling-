78be54b0c6115df7ee00d44db3b7eac7caf42dfc
SonaliSinha <sonalisinha22@gmail.com>
2020-12-03 13:39:44 +0530

    new keyword added

--- sketch.js
+++ sketch.js
@@ -18,7 +18,7 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20);
-	platform = Ground(150,305,300,170);
+	platform = new Ground(150,305,300,170);
 
     box1 = new Box(700,320,70,70);
     
@@ -49,7 +49,7 @@ function draw(){
     box1.display();
     box2.display();
     ground.display();
-	platform.display();
+    platform.display();
     pig1.display();
     log1.display();
 
