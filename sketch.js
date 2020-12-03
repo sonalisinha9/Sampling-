cc42626114494feb78e53f6b3f03a957a68943fe
SonaliSinha <sonalisinha22@gmail.com>
2020-12-03 13:31:09 +0530

    Platform Added

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg;
+var platform;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -16,10 +17,12 @@ function setup(){
     world = engine.world;
 
     
-    ground = new Ground(600,height,1200,20)
+    ground = new Ground(600,height,1200,20);
+	platform = Ground(150,305,300,170);
 
     box1 = new Box(700,320,70,70);
-    box2 = new Box(920,320,70,70);
+    
+	box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
 
@@ -46,6 +49,7 @@ function draw(){
     box1.display();
     box2.display();
     ground.display();
+	platform.display();
     pig1.display();
     log1.display();
 
